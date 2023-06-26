46b9922224eefa6def4241049811bcf2440285de
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-06-26 20:09:58 +0100

    Insertion Sort List

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,31 +1,6 @@
 #include "sort.h"
 
 /**
- * swapNodes - This function is to swap nodes
- * @a: 1st node
- * @b: 2nd node
- * @head: pointer to the head
- * Return: void
- */
-void swapNodes(listint_t **head, listint_t *a, listint_t *b)
-{
-	if (a->prev)
-		a->prev->next = b;
-	else
-		*head = b;
-
-	if (b->next)
-		b->next->prev = a;
-
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = b;
-	b->next = a;
-
-	print_list(*head);
-}
-
-/**
  * insertion_sort_list - This sort according to the jion
  * algorithm
  * @list: pointer to the head
@@ -45,7 +20,17 @@ void insertion_sort_list(listint_t **list)
 
 		while (tmp->prev && tmp->n < tmp->prev->n)
 		{
-			swapNodes(list, tmp->prev, tmp);
+			tmp->prev->next = tmp->next;
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->next->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev)
+				tmp->prev->next = tmp;
+			else
+				*list = tmp;
+			print_list(*list);
 		}
 	}
 }
