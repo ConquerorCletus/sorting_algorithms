f0bc684e6540826c8d46c5eef4bbeeda013128b7
ConquerorCletus <smartchibuike2019@gmail.com>
2023-06-27 13:20:01 +0000

    shell sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,13 +7,12 @@
  */
 void swap(int *a, int *b)
 {
-        int tmp;
+	int tmp;
 
-        tmp = *a;
-        *a = *b;
-        *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
-
 /**
  * shell_sort - Sorts an array of integers
  * using the shell sort algorithm.
@@ -22,26 +21,26 @@ void swap(int *a, int *b)
  */
 void shell_sort(int *array, size_t size)
 {
-        size_t gap, i, j;
+	size_t gap, i, j;
 
-        if (array == NULL || size < 2)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        for (gap = 1; gap < (size / 3);)
-                gap = gap * 3 + 1;
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-        for (; gap >= 1; gap /= 3)
-        {
-                for (i = gap; i < size; i++)
-                {
-                        j = i;
-                        while (j >= gap && array[j - gap] > array[j])
-                        {
-                                swap(array + j, array + (j - gap));
-                                j -= gap;
-                        }
-                }
-                print_array(array, size);
-        }
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
 }
 
